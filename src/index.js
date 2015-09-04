5a135e68a5069db4300cc3526e4619dbe77c65a5
Asaf Shakarzy <asaf@indexia.co>
2015-09-04 23:40:46 +0300

    File case sensitivity bug fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import notifReducer from './reducers/notifs'
 import * as notifActions from './actions/notifs'
-import NotifsComponent from './components/notifs'
+import NotifsComponent from './components/Notifs'
 
 export {
   notifReducer, notifActions, NotifsComponent
