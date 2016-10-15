1aab66d5d1a2fff6404456ec6b0f91b04f47eb38
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-10-15 12:01:11 -0700

    working demo, updated babel

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -5,23 +5,20 @@ var npmPath = path.resolve(__dirname, '../node_modules');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   cache: true,
-  entry: [
-      'webpack-hot-middleware/client',
-      './index',
-      './index.html'
-  ],
+  entry: {
+    demo: ['./demo.js']
+  },
   output: {
-    path: path.join(__dirname, 'dist_demo'),
-    filename: 'demo.js',
-    publicPath: '/static/'
+    path: path.resolve(__dirname, 'dist_demo'),
+    publicPath: '/static/',
+    filename: 'bundle.js'
   },
   resolveLoader: { modulesDirectories: [npmPath] },
   resolve: {
     extensions: ['', '.css', '.js', '.json', '.jsx', '.webpack.js', '.web.js'],
   },
   plugins: [
-    // new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.NormalModuleReplacementPlugin(
       /^re-notif/, function(data) {
