cc39eff4a665f38bf0cd3cd4ad1e332370b276bc
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-10-14 20:46:08 -0700

    update linting

--- src/components/Notif.js
+++ src/components/Notif.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Notif = ({ kind, componentClassName, actionLabel, onActionClick, id, message }) => {
-  const _onActionClick = (ev) => {
+  const handleActionClick = (ev) => {
     ev.preventDefault();
 
     if (!onActionClick) {
@@ -13,16 +13,16 @@ const Notif = ({ kind, componentClassName, actionLabel, onActionClick, id, messa
 
   return (
     <div className={`${componentClassName} ${componentClassName}--${kind}`}>
-        <div className={`${componentClassName}__icon`} />
-        <div className={`${componentClassName}__content`}>
-          <span className={`${componentClassName}__message`}>{message}</span>
-        </div>
-        { actionLabel &&
-          <span className={`${componentClassName}__action`}>
-            <button onClick={_onActionClick}>{actionLabel}</button>
-          </span>
-        }
-        <div className={`${componentClassName}__close`} />
+      <div className={`${componentClassName}__icon`} />
+      <div className={`${componentClassName}__content`}>
+        <span className={`${componentClassName}__message`}>{message}</span>
+      </div>
+      {actionLabel &&
+        <span className={`${componentClassName}__action`}>
+          <button onClick={handleActionClick}>{actionLabel}</button>
+        </span>
+      }
+      <div className={`${componentClassName}__close`} />
     </div>
   );
 };
